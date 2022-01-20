b9e4f7f0dd17046339e42a1802f7c0ee81700dba
brakesh <bandarupallirakesh@gmail.com>
2022-01-20 21:33:35 +0530

    Shared library updated

--- ec2.tf
+++ ec2.tf
@@ -16,24 +16,3 @@ resource "aws_ec2_tag" "spot_instance" {
   key="Name"
   value="${var.COMPONENT}-${var.ENV}-${count.index+1}"
 }
-resource "aws_lb_target_group" "tg" {
-  name= "${var.ENV}-${var.COMPONENT}"
-  port= 80
-  protocol = "HTTP"
-  vpc_id=data.terraform_remote_state.vpc.outputs.VPC_ID
-}
-resource "aws_lb_target_group_attachment" "tg-attach" {
-  count=length(aws_spot_instance_request.ec2-spot)
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id        = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
-  port=80
-}
-resource "aws_lb_listener" "lb-listener" {
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LB_ARN
-  port="80"
-  protocol="HTTP"
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
