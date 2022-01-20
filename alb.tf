6894d65a975a95c09ea4bd7142635c6de4f828f5
brakesh <bandarupallirakesh@gmail.com>
2022-01-20 22:00:04 +0530

    Shared library updated

--- alb.tf
+++ alb.tf
@@ -1,14 +1,23 @@
 resource "aws_lb_target_group" "tg" {
   name= "${var.ENV}-${var.COMPONENT}"
-  port= 80
+  port= var.APP_PORT
   protocol = "HTTP"
   vpc_id=data.terraform_remote_state.vpc.outputs.VPC_ID
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    path="/health"
+    port=var.APP_PORT
+    timeout=3
+  }
 }
 resource "aws_lb_target_group_attachment" "tg-attach" {
   count=length(aws_spot_instance_request.ec2-spot)
   target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_spot_instance_request.ec2-spot.*.spot_instance_id[count.index]
-  port=80
+  port=var.APP_PORT
 }
 resource "aws_lb_listener" "lb-listener" {
 count=var.LB_PUBLIC?1:0
